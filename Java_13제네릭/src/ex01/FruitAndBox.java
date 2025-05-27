4672d17b765ff4d270189124013229646e5a56da
dino-21 <80396471+dino-21@users.noreply.github.com>
2025-05-27 12:11:59 +0900

    Update FruitAndBox.java

--- Java_13제네릭/src/ex01/FruitAndBox.java
+++ Java_13제네릭/src/ex01/FruitAndBox.java
@@ -40,17 +40,17 @@ class OrangeBox{
 }
 
 public class FruitAndBox {
-
+	//제네릭이 없으면 타입마다 전용 클래스를 만들어야 하는 불편함
 	public static void main(String[] args) {
 		
 		AppleBox aBox = new AppleBox();
 		OrangeBox oBox = new OrangeBox();
 		
-		aBox.setAp(new Apple());	
-		oBox.setOp(new Orange());
+		aBox.setAp(new Apple());  // 사과 상자에 사과 넣기
+		oBox.setOp(new Orange()); // 오렌지 상자에 오렌지 넣기
 		
-		Apple ap = aBox.getAp();
-		Orange op =oBox.getOp();
+		Apple ap = aBox.getAp();  //사과박스(aBox)에서 사과 꺼내기
+		Orange op =oBox.getOp();  //오렌지박스(oBox)에서 오렌지 꺼내기
 		
 		System.out.println(ap);
 		System.out.println(op);
