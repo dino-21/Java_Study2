3aed8553b7793395a6e7bbb2efd4652073beee90
dino-21 <80396471+dino-21@users.noreply.github.com>
2025-05-27 09:49:28 +0900

    Update FinalExam.java

--- Java_07클래스/src/ex07/FinalExam.java
+++ Java_07클래스/src/ex07/FinalExam.java
@@ -3,9 +3,10 @@ package ex07;
 import java.util.Scanner;
 
 import ex06.MathArrayUtils;
-
+//final로 선언된 클래스로, 상속이 불가능
+//주로 상수나 기능을 고정시킨 클래스를 만들 때 사용
 public final class  FinalExam {
-
+// MAX는 한 번만 초기화 가능한 상수 변수
 	private final int MAX;
 	
 	FinalExam(int n){
@@ -25,7 +26,8 @@ public final class  FinalExam {
 		
 		
 	}
-	
+	//final 키워드를 메서드에 붙이면 
+	// 이 메서드는 자식 클래스에서 오버라이딩(재정의)할 수 없다는 의미
 	private final void func() {
 		
 	}
