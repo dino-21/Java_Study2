f932e5f4c2ab06dacd5536c215770a4b4a78a43d
dino-21 <80396471+dino-21@users.noreply.github.com>
2025-05-27 10:46:26 +0900

    Update Main.java

--- Java_10종합/src/ex07/Main.java
+++ Java_10종합/src/ex07/Main.java
@@ -9,8 +9,11 @@ public class Main {
 		c.info(); // 10 20 30 출력
 		
 		func(new ClassA());
+		System.out.println("===1");
 		func(new ClassB());
+		System.out.println("===2");
 		func(new ClassC());
+		System.out.println("===3");
 		
 		
 	}
@@ -21,6 +24,7 @@ public class Main {
 //		ClassA a1 = new ClassB();
 //		ClassA a2 = new ClassC();
 		System.out.println();
+		//ClassB 또는 그 하위 클래스의 인스턴스
 		if(a instanceof ClassB) {
 			System.out.println("aaa");			
 		}
