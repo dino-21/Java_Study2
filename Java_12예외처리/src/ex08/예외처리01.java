486c290d50fcb3fc34fc23318076974249d67bc2
dino-21 <80396471+dino-21@users.noreply.github.com>
2025-05-27 11:56:18 +0900

    Update 예외처리01.java

--- Java_12예외처리/src/ex08/예외처리01.java
+++ Java_12예외처리/src/ex08/예외처리01.java
@@ -3,7 +3,8 @@ package ex08;
 import java.util.Scanner;
 
 public class 예외처리01 {
-
+        // 예외가 발생하면 JVM(자바 실행기)에게 던질게,  예외가 발생하면 프로그램이 바로 종료됨
+	// catch 블록이 없기 때문에 예외는 처리되지 않음
 	public static void main(String[] args) throws Exception {
 		
 		
